dd74366ecdd33a3ad3e94c5e4799ef21bc984abe
강춘배 <bilvin@gangchunbaeui-MacBookAir.local>
2023-01-09 19:49:24 +0900

    20230109-PM0749

--- Server/app.js
+++ Server/app.js
@@ -1,14 +1,20 @@
 const wsServer = require("ws").Server;
-const wss = new wsServer({ port: 3000 });
+const wss = new wsServer({ port: 3000 }, () => {
+    console.log("server is running port 3000")
+});
 
-
+let data = {
+    type: "message",
+    id: "admin",
+    pw: "adminpw"
+}
 
 wss.on('connection', (ws, request) => {
     console.log("client connected");
     ws.on('message', (msg) => {
         console.log("client: %s", msg);
     })
-
+    ws.send(JSON.stringify(data));
     // client.send("hello!");
 
     const ip = request.headers['x-forwarded-for'] || request.connection.remoteAddress;
