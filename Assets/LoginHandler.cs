bd72507132c5a0fd488a295c620378ccdcd5d658
강춘배 <bilvin@gangchunbaeui-MacBookAir.local>
2023-01-17 00:55:04 +0900

    20230117-AM1255

--- Assets/LoginHandler.cs
+++ Assets/LoginHandler.cs
@@ -11,64 +11,64 @@ public class LoginHandler : MonoBehaviour
 {
     public TMP_InputField inputID;
     public TMP_InputField inputPW;
-    // private WebSocket ws;
-    // private string msgData;
-    // void Start()
-    // {
-    //     ws = new WebSocket("ws://127.0.0.1:8484");
-    //     ws.OnMessage += ws_OnMessage;
-    //     ws.OnOpen += ws_OnOpen;
-    //     ws.OnClose += ws_OnClose;
-    //     ws.Connect();
-    // }
-
-    // void ws_OnMessage(object sender, MessageEventArgs e)
-    // {
-    //     msgData = e.Data;
-    // }
-    // void ws_OnOpen(object sender, System.EventArgs e)
-    // {
-    //     Debug.Log("Connecting to login server...");
-    // }
-    // void ws_OnClose(object sender, CloseEventArgs e)
-    // {
-    //     Debug.Log("Lost connection with login server");
-    //     // Start();
-    // }
-    // public void clickLoginBT()
-    // {
-    //     UserInfo userInfo = new UserInfo();
-    //     userInfo.userID = inputID.text;
-    //     userInfo.userPW = inputPW.text;
-
-    //     ws.Send(JsonUtility.ToJson(userInfo));
-    //     Debug.Log(JsonUtility.ToJson(userInfo));
-    //     inspectLogin();
-    // }
-    // public void goToSignup()
-    // {
-    //     // ws.Close();
-    //     // SceneManager.LoadScene("SignupScene");
-    //     Debug.Log(msgData);
-    // }
-    // public void inspectLogin()
-    // {
-    //     if (msgData == "OK")
-    //     {
-    //         Debug.Log("Hmm...");
-    //         SceneManager.LoadScene("MainScene");
-    //     }
-    //     else if (msgData == "NO")
-    //     {
-    //         Debug.Log("NONONONONO");
-    //     }
-
-    // }
-    private SocketManager socket = null;
+    private WebSocket ws;
+    private string msgData;
     void Start()
     {
-        Debug.Log(socket);
+        ws = new WebSocket("ws://127.0.0.1:8484");
+        ws.OnMessage += ws_OnMessage;
+        ws.OnOpen += ws_OnOpen;
+        ws.OnClose += ws_OnClose;
+        ws.Connect();
     }
+
+    void ws_OnMessage(object sender, MessageEventArgs e)
+    {
+        msgData = e.Data;
+    }
+    void ws_OnOpen(object sender, System.EventArgs e)
+    {
+        Debug.Log("Connecting to login server...");
+    }
+    void ws_OnClose(object sender, CloseEventArgs e)
+    {
+        Debug.Log("Lost connection with login server");
+        // Start();
+    }
+    public void clickLoginBT()
+    {
+        UserInfo userInfo = new UserInfo();
+        userInfo.userID = inputID.text;
+        userInfo.userPW = inputPW.text;
+
+        ws.Send(JsonUtility.ToJson(userInfo));
+        Debug.Log(JsonUtility.ToJson(userInfo));
+        inspectLogin();
+    }
+    public void goToSignup()
+    {
+        // ws.Close();
+        // SceneManager.LoadScene("SignupScene");
+        Debug.Log(msgData);
+    }
+    public void inspectLogin()
+    {
+        if (msgData == "OK")
+        {
+            Debug.Log("Hmm...");
+            SceneManager.LoadScene("MainScene");
+        }
+        else if (msgData == "NO")
+        {
+            Debug.Log("NONONONONO");
+        }
+
+    }
+    // private SocketManager socket = null;
+    // void Start()
+    // {
+    //     Debug.Log(socket);
+    // }
 }
 
 public class UserInfo
